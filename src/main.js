1b58a965c0b381c12b6cda1e2f4003b4f6cdf6bf
logonzalez <lalzate@enerbit.co>
2023-12-29 17:07:17 -0500

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import  css from './main.css'
 
 import {router} from './router/index.routes'
 
-router(window.location.hash)
-window.addEventListener('hashchange',() =>{
-    router(window.location.hash)
-} )
+const init = () => {
+    router(window.location.hash);
+  
+    window.addEventListener("hashchange", () => {
+      router(window.location.hash);
+    });
+  };
+  
+  window.addEventListener("load", init);
