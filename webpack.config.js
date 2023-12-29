b73acff287d3a7bafba2e1c595f70c79568cdcb9
logonzalez <lalzate@enerbit.co>
2023-12-29 17:12:39 -0500

    update deploy config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'production',
     entry: './src/main.js',
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "../dist"),
         filename: 'bundle.js',
     },
     module: {
